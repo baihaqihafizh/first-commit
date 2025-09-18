0b376343cffc573ce56c8fbb24f57da7be683131
baihaqihafizh <baihaqihafizhabrorni61@gmail.com>
2025-09-18 13:07:57 +0700

    3_type_data.dart

---
+++ 3_type_data.dart
@@ -0,0 +1,63 @@
+/**
+ * Variabel adalah suatu wadah untuk
+ * menyimpan data  
+ */ 
+
+/**
+ * Tipe data adalah suatu konsep yang digunakan
+ * untuk mengklasifikasikan jenis data
+ * yang dapat diproses oleh progam
+ */
+
+/**
+ * jenis tipe data
+ * 
+ * int => integer => bilangan bulat 
+ * contoh 1, -2, 11 dsb / dan sebagainya
+ */
+
+/**
+ * double => bilangan desimal
+ * contoh => 3.14, -0.1, 2.5 dsb / dan sebagainya
+ */
+
+/**
+ * num => gabungan dari integer dan double
+ * 3.14, 2, -2, 0, 3.15 dsb / dan sebagainya
+ */
+
+/**
+ * bool = boolean = true or false
+ */
+
+/**
+ * String =  string => "kumpulan karakter"
+ */
+
+/**
+ * List = berupa daftar
+ * [1, 2 dsb ]
+ * [a' b' dsb]
+ */
+
+/**
+ * Map => berupa key:value
+ * {"nama kota : solo"}
+ */
+
+/**
+ * Dynamic => campuran / tipe apapun
+ */
+
+void main(List<String> args) {
+  String nama = "bha";
+  int age = 14;
+  
+  var tinggi_badan;
+  tinggi_badan = 159;;
+  tinggi_badan = "Seratus lima semblilan";
+
+  bool isMaried = false;
+  double berat_badan = 62.7;
+  
+}
