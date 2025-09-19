c1816c63024a7fff9b15bdcde32c97fa61a5587b
baihaqihafizh <baihaqihafizhabrorni61@gmail.com>
2025-09-19 09:12:15 +0700

    Belajar num

---
+++ 5_num.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  // num itu bisa tipe data number (int dan double)
+  num a = 10;
+  num b = 10.5;
+  num hex = 0xDEADBEEF;
+
+  print(a);
+  print(b);
+  print(hex);
+}
