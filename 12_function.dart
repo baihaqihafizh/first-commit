d09f8c6b621356a4c2b89e2afb150fe1716d06dc
baihaqihafizh <baihaqihafizhabrorni61@gmail.com>
2025-09-23 14:17:34 +0700

    aroow fuction

--- 12_function.dart
+++ 12_function.dart
@@ -15,8 +15,11 @@ void main(List<String> args) {
   // memanggil function
   birthDay();
   birthDay2("Ucup", "Baini", 13);
-  birthday3("Ucup", age: 13,);
-  birthday3("Ucup", weight:  20.5,);
+  birthday3("Ucup", age: 13);
+  birthday3("Ucup", weight: 20.5);
+
+  print("Keliling Lingkaran: ${kelilingLingkaran(10)}");
+  print("Luas Lingkaran: ${luasLingkaran(10)}");
 }
 
 // function tampa return value
@@ -40,3 +43,23 @@ void birthday3(String name, {int age = 17, double weight = 50.5}) {
 // ataupun tidak mengisi parameter optional
 // maka akan menggunakan nilai default
 
+/**
+ * arroe fungtion 
+ * with return valeu
+ */
+
+/**
+ * num adalah tipe data unutk angka
+ * nama function [luasLingkaran]
+ * memiliki paramater r (jari - jari)
+ */
+
+// kita buat var golbal
+const pi = 3.14;
+
+num luasLingkaran(num r) {
+  return 3.14 * r * r;
+}
+
+// errow function
+num kelilingLingkaran(num r) => 2 * pi * r;
